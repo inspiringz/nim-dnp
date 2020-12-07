224dead5641423a5167579e19d8ea6021ee012ae
inspiringz <z@3nd.xyz>
2020-12-07 20:32:45 +0800

    update

--- dnp.nim
+++ dnp.nim
@@ -2,18 +2,23 @@ import os
 import times
 import streams
 import strutils
-import parsecfg
+#import parsecfg
 import argparse
 import sequtils
 import itertools
 
-
+#[
 let
   Section = ["environment", "role", "technology", "service", "monitor",
     "proxy", "version", "time", "space", "area", "application", "number"]
 
 type
   Config = array[Section.len, seq[string]]
+]#
+
+var config_simple: seq[seq[string]] = @[@["dev", "pre", "test", "t1", "staging", "st", "st1", "pro", "prod", "uat"], @["qa", "fe", "backend", "be"], @["api", "apis"], @["rest", "html5", "h5", "web", "app", "apps", "wx", "weixin", "m"], @["monitor", "dashboard"], @["proxy", "corp", "internal", "lb", "nginx", "gateway"], @["demo", "alpha", "beta", "stable", "release", "ga", "rc"], @["preview", "previous", "new", "new1", "old"], @["east", "south", "west", "north", "southeast", "northwest", "es", "nt", "tx", "tencent", "ali", "aliyun", "aws", "center"], @["huadong", "hd", "huaxi", "hx", "huanan", "hn", "huabei", "hb", "japan", "jp", "hongkong", "hk", "shanghai", "sh", "beijing", "bj", "us"], @["dc", "es", "h5", "mq", "oa", "uc", "zk", "api", "apm", "bbs", "biw", "bot", "bus", "cat", "cms", "crm", "dc1", "doc", "ec2", "efk", "elk", "gce", "git", "hub", "job", "jpa", "jwt", "k8s", "lib", "log", "mbs", "mgt", "mvc", "nms", "pan", "pay", "pod", "rpc", "scm", "srv", "sso", "svn", "tms", "wms", "www", "amqp", "apis", "app1", "apps", "auth", "avro", "blog", "boot", "data", "docs", "file", "flex", "gogs", "good", "grid", "guns", "hdfs", "hive", "kube", "ldap", "live", "logs", "mail", "mqtt", "nifi", "node", "note", "open", "pipe", "pods", "repo", "shop", "show", "solr", "team", "tsdb", "user", "wiki", "work", "zuul", "admin", "apiv1", "apiv2", "apiv3", "apiv4", "apiv5", "app01", "azure", "batch", "beats", "cacti", "camel", "chaos", "dubbo", "event", "feign", "flink", "flume", "geode", "gitea", "goods", "graph", "group", "habor", "hbase", "html5", "infra", "kafka", "label", "maven", "mesos", "micro", "minio", "nacos", "neo4j", "nerve", "nexus", "node1", "oauth", "oozie", "redis", "route", "scala", "spark", "sqoop", "stack", "store", "storm", "webui", "api-v1", "api-v2", "api-v3", "api-v4", "api-v5", "center", "client", "consul", "devops", "docker", "eureka", "falcon", "galaxy", "github", "gitlab", "goblin", "gradle", "group1", "hadoop", "harbor", "influx", "kibana", "lcinga", "logapi", "logger", "manage", "mobile", "nagios", "node01", "oauth2", "office", "pgraph", "portal", "ribbon", "router", "scribe", "search", "server", "sleuth", "spark1", "splunk", "spring", "stream", "syslog", "sysmon", "tracer", "triton", "tuning", "web-ui", "weblog", "zabbix", "zipkin", "airflow", "akumuli", "ansible", "bigdata", "breaker", "brogmon", "catalog", "circuit", "content", "control", "datadog", "diagram", "elastic", "eureka1", "fluentd", "gateway", "gemfire", "grafana", "hystrix", "invoker", "jenkins", "jupyter", "kinesis", "library", "logging", "manager", "meeting", "metrics", "monitor", "netdata", "netflix", "nodered", "payment", "pushapi", "recruit", "restapi", "restful", "rsyslog", "service", "storage", "tracing", "turbine", "webflow", "activemq", "actuator", "cadvisor", "collectd", "contract", "dataflow", "elkstack", "exporter", "filebeat", "graphite", "heapster", "influxdb", "librenms", "logstash", "marathon", "node-red", "opentsdb", "pipeline", "platform", "push-api", "rabbitmq", "schedule", "searcher", "tracking", "training", "analytics", "atlassian", "cassandra", "community", "dashboard", "developer", "discovery", "dockerhub", "dzzoffice", "elk-stack", "kubernete", "logsearch", "mapreduce", "office365", "openstack", "pagerduty", "serverset", "terraform", "websocket", "zookeeper", "clickhouse", "cloudwatch", "confluence", "dashboards", "datacenter", "dispatcher", "kube-state", "kubernetes", "management", "openfalcon", "prometheus", "servicelog", "skywalking", "usercenter", "data-center", "eureka-zuul", "kube-status", "loganalysis", "open-falcon", "opentracing", "pushgateway", "recruitment", "restful-api", "service-log", "serviceslog", "user-center", "alertmanager", "eureka-admin", "grafana-kong", "push-gateway", "services-log", "elasticsearch", "eureka-client", "eureka-server", "node-exporter", "kong-dashboard", "grafana-dashboard", "grafana-management"], @["1", "2", "3", "01", "02", "03", "v1", "v2", "v3"]]
+
+var config_default: seq[seq[string]] = @[@["dev", "pre", "prepare", "test", "test1", "t1", "staging", "st", "st1", "prd", "pro", "prod", "p1", "pr", "uat", "sit"], @["qa", "qa1", "qa3", "front", "frontend", "fe", "backend", "be"], @["api", "apis"], @["rest", "restful", "html5", "h5", "web", "app", "apps", "wx", "weixin", "mobile", "m", "wap"], @["admin", "monitor", "manager", "dashboard", "management"], @["proxy", "corp", "internal", "cluster", "cluster1", "lb", "nginx", "ng", "gateway", "gw"], @["demo", "alpha", "beta", "stable", "release", "ga", "rc"], @["preview", "previous", "new", "new1", "old"], @["east", "south", "west", "north", "southeast", "northwest", "es", "nt", "tx", "tencent", "ali", "aliyun", "aws", "center"], @["huadong", "hd", "huaxi", "hx", "huanan", "hn", "huabei", "hb", "japan", "jp", "hongkong", "hk", "shanghai", "sh", "beijing", "bj", "us"], @["dc", "es", "h5", "mq", "oa", "uc", "zk", "api", "apm", "bbs", "biw", "bot", "bus", "cat", "cms", "crm", "dc1", "doc", "ec2", "efk", "elk", "gce", "git", "hub", "job", "jpa", "jwt", "k8s", "lib", "log", "mbs", "mgt", "mvc", "nms", "pan", "pay", "pod", "rpc", "scm", "srv", "sso", "svn", "tms", "wms", "www", "amqp", "apis", "app1", "apps", "auth", "avro", "blog", "boot", "data", "docs", "file", "flex", "gogs", "good", "grid", "guns", "hdfs", "hive", "kube", "ldap", "live", "logs", "mail", "mqtt", "nifi", "node", "note", "open", "pipe", "pods", "repo", "shop", "show", "solr", "team", "tsdb", "user", "wiki", "work", "zuul", "admin", "apiv1", "apiv2", "apiv3", "apiv4", "apiv5", "app01", "azure", "batch", "beats", "cacti", "camel", "chaos", "dubbo", "event", "feign", "flink", "flume", "geode", "gitea", "goods", "graph", "group", "habor", "hbase", "html5", "infra", "kafka", "label", "maven", "mesos", "micro", "minio", "nacos", "neo4j", "nerve", "nexus", "node1", "oauth", "oozie", "redis", "route", "scala", "spark", "sqoop", "stack", "store", "storm", "webui", "api-v1", "api-v2", "api-v3", "api-v4", "api-v5", "center", "client", "consul", "devops", "docker", "eureka", "falcon", "galaxy", "github", "gitlab", "goblin", "gradle", "group1", "hadoop", "harbor", "influx", "kibana", "lcinga", "logapi", "logger", "manage", "mobile", "nagios", "node01", "oauth2", "office", "pgraph", "portal", "ribbon", "router", "scribe", "search", "server", "sleuth", "spark1", "splunk", "spring", "stream", "syslog", "sysmon", "tracer", "triton", "tuning", "web-ui", "weblog", "zabbix", "zipkin", "airflow", "akumuli", "ansible", "bigdata", "breaker", "brogmon", "catalog", "circuit", "content", "control", "datadog", "diagram", "elastic", "eureka1", "fluentd", "gateway", "gemfire", "grafana", "hystrix", "invoker", "jenkins", "jupyter", "kinesis", "library", "logging", "manager", "meeting", "metrics", "monitor", "netdata", "netflix", "nodered", "payment", "pushapi", "recruit", "restapi", "restful", "rsyslog", "service", "storage", "tracing", "turbine", "webflow", "activemq", "actuator", "cadvisor", "collectd", "contract", "dataflow", "elkstack", "exporter", "filebeat", "graphite", "heapster", "influxdb", "librenms", "logstash", "marathon", "node-red", "opentsdb", "pipeline", "platform", "push-api", "rabbitmq", "schedule", "searcher", "tracking", "training", "analytics", "atlassian", "cassandra", "community", "dashboard", "developer", "discovery", "dockerhub", "dzzoffice", "elk-stack", "kubernete", "logsearch", "mapreduce", "office365", "openstack", "pagerduty", "serverset", "terraform", "websocket", "zookeeper", "clickhouse", "cloudwatch", "confluence", "dashboards", "datacenter", "dispatcher", "kube-state", "kubernetes", "management", "openfalcon", "prometheus", "servicelog", "skywalking", "usercenter", "data-center", "eureka-zuul", "kube-status", "loganalysis", "open-falcon", "opentracing", "pushgateway", "recruitment", "restful-api", "service-log", "serviceslog", "user-center", "alertmanager", "eureka-admin", "grafana-kong", "push-gateway", "services-log", "elasticsearch", "eureka-client", "eureka-server", "node-exporter", "kong-dashboard", "grafana-dashboard", "grafana-management"], @["1", "2", "3", "01", "02", "03", "v1", "v2", "v3"]]
 
 proc seqToString(seq_string: seq[string], js: string=""): string =
   for i in low(seq_string)..high(seq_string):
@@ -46,7 +51,7 @@ proc read_all_domains(input_file: string): seq[string] =
   result = sequtils.deduplicate(domains, isSorted=true)
   return result
 
-
+#[
 proc get_configuration(current_dir: string, mode: string): seq[seq[string]] =
   var
     config: Config
@@ -77,7 +82,7 @@ proc get_configuration(current_dir: string, mode: string): seq[seq[string]] =
       echo("[-] cannot open: " & cfg)
   
   return @config
-
+]#
 
 iterator awesome_factory(join_string: string, elements: seq[seq[string]], mix_join: string = ""): string =
   var element_num: int
@@ -251,8 +256,13 @@ when isMainModule:
   
   if not silent_mode: echo ascii_banner
   
-  var 
-    config = get_configuration(current_dir, predictor_mode)
+  # var config = get_configuration(current_dir, predictor_mode)
+  var config: seq[seq[string]]
+  if predictor_mode == "simple":
+    config = config_simple
+  else:
+    config = config_default
+
 
   var
     count = 0
@@ -271,7 +281,6 @@ when isMainModule:
       else: file.writeLine(i)
       count += 1
   else:
-    var join_results: seq[string] = @[]
     for ns in read_all_domains(input_file):
       if(is_main_domain(ns)):
         for i in get_main_domain_similar_domain_names(ns, config):
